d63b2668519cea424ddb4c1ecfb7af3ccd5efaf5
municorn <municorn@musicaloft.com>
2020-08-23 07:22:18 -0600

    wpg: Add post_load.sh script
    This script will reload Awesome on its own and also generate the lock
    screen wallpaper (for faster locking)

---
+++ wpg/post_load.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+killall muse-status
+killall muse-status-dae
+
+awesome-client "awesome.restart()"
+
+tmpbg='/tmp/screen.png'
+
+cp $HOME/.config/wpg/wallpapers/$(wpg -c) $tmpbg
+/usr/bin/convert "$tmpbg" \
+    -resize 1920x1080^ \
+    -gravity center \
+    -extent 1920x1080 \
+    -fill "#0A090A" \
+    -colorize 75% \
+    "$tmpbg"
+
