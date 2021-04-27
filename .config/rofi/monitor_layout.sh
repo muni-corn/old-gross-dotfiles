bca48b8ba29730fecdd6f869fa9cb89d33a26eca
municorn <municorn@musicaloft.com>
2021-04-27 01:18:35 -0600

    Add rofi

---
+++ .config/rofi/monitor_layout.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+XRANDR=$(which xrandr)
+
+MONITORS=( $( ${XRANDR} | awk '( $2 == "connected" ){ print $1 }' ) )
+
+
+NUM_MONITORS=${#MONITORS[@]}
+
+TITLES=()
+COMMANDS=()
+
+
+function gen_xrandr_only()
+{
+    selected=$1
+
+    cmd="xrandr --output ${MONITORS[$selected]} --auto "
+
+    for entry in $(seq 0 $((${NUM_MONITORS}-1)))
+    do
+        if [ $selected != $entry ]
+        then
+            cmd="$cmd --output ${MONITORS[$entry]} --off"
+        fi
+    done
+
+    echo $cmd
+}
+
+
+declare -i index=0
+TILES[$index]="Cancel"
+COMMANDS[$index]="true"
+index+=1
+
+
+for entry in $(seq 0 $((${NUM_MONITORS}-1)))
+do
+    TILES[$index]="Only ${MONITORS[$entry]}"
+    COMMANDS[$index]=$(gen_xrandr_only $entry)
+    index+=1
+done
+
+##
+# Dual screen options
+##
+for entry_a in $(seq 0 $((${NUM_MONITORS}-1)))
+do
+    for entry_b in $(seq 0 $((${NUM_MONITORS}-1)))
+    do
+        if [ $entry_a != $entry_b ]
+        then
+            TILES[$index]="Dual ${MONITORS[$entry_a]} →  ${MONITORS[$entry_b]}"
+            COMMANDS[$index]="xrandr --output ${MONITORS[$entry_a]} --auto \
+                              --output ${MONITORS[$entry_a]} --auto --left-of ${MONITORS[$entry_b]}"
+
+            index+=1
+        fi
+    done
+done
+
+
+##
+# Clone monitors
+##
+for entry_a in $(seq 0 $((${NUM_MONITORS}-1)))
+do
+    for entry_b in $(seq 0 $((${NUM_MONITORS}-1)))
+    do
+        if [ $entry_a != $entry_b ]
+        then
+            TILES[$index]="Clone ${MONITORS[$entry_a]} → ${MONITORS[$entry_b]}"
+            COMMANDS[$index]="xrandr --output ${MONITORS[$entry_a]} --auto \
+                              --output ${MONITORS[$entry_a]} --auto --same-as ${MONITORS[$entry_b]}"
+
+            index+=1
+        fi
+    done
+done
+
+
+##
+#  Generate entries, where first is key.
+##
+function gen_entries()
+{
+    for a in $(seq 0 $(( ${#TILES[@]} -1 )))
+    do
+        echo -e "$a\t${TILES[a]}"
+    done
+}
+
+# Call menu
+SEL=$( gen_entries | rofi -dmenu -p "Monitor layout" -a 0 -no-custom  | awk '{print $1}' )
+
+# Call xrandr
+$( ${COMMANDS[$SEL]} )
