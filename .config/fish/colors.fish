06cf4ed844be2c05e2f7a25ff6884f7814ef94c7
municorn <municorn@musicaloft.com>
2021-06-24 23:46:17 -0600

    fish: Add colors.fish

---
+++ .config/fish/colors.fish
@@ -0,0 +1,25 @@
+set -U fish_color_autosuggestion blue
+set -U fish_color_cancel -r
+set -U fish_color_command brwhite --bold
+set -U fish_color_comment white
+set -U fish_color_cwd magenta
+set -U fish_color_cwd_root red
+set -U fish_color_end blue
+set -U fish_color_error ff0000
+set -U fish_color_escape brgreen
+set -U fish_color_history_current --bold
+set -U fish_color_host normal
+set -U fish_color_match --background=brblue
+set -U fish_color_normal normal
+set -U fish_color_operator brcyan
+set -U fish_color_param bryellow
+set -U fish_color_quote yellow
+set -U fish_color_redirection magenta
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_user brgreen
+set -U fish_color_valid_path --underline
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description yellow bryellow
+set -U fish_pager_color_prefix white --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
