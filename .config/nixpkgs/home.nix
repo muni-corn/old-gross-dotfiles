a5881c4f991eeaa40b8062ad4b1770a7b1ffd566
municorn <municorn@musicaloft.com>
2021-07-04 19:18:02 -0600

    nixpkgs: Make `home` a block

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -128,16 +128,18 @@
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
-  home.username = "municorn";
-  home.homeDirectory = "/home/municorn";
+  home = {
+    username = "municorn";
+    homeDirectory = "/home/municorn";
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "21.11";
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "21.11";
+  };
 }
