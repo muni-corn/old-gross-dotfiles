5323f1543c98f787361299ccc3026ca735330d8b
municorn <municorn@musicaloft.com>
2021-07-13 19:17:44 -0600

    hm: Add and switch to Solarized Dark colors

--- .config/nixpkgs/colors.nix
+++ .config/nixpkgs/colors.nix
@@ -1,24 +1,43 @@
 rec {
-  # monochrome colors
   active = color15;
   inactive = color07;
 
-  color00 = "000000";
-  color08 = "111111";
+  # monochrome colors
 
-  color01 = "222222";
-  color02 = "333333";
-  color03 = "444444";
-  color04 = "555555";
-  color05 = "666666";
-  color06 = "777777";
-  color09 = "888888";
-  color10 = "999999";
-  color11 = "aaaaaa";
-  color12 = "bbbbbb";
-  color13 = "cccccc";
-  color14 = "dddddd";
+  # color00 = "000000";
+  # color08 = "111111";
 
-  color07 = "eeeeee";
-  color15 = "ffffff";
+  # color01 = "222222";
+  # color02 = "333333";
+  # color03 = "444444";
+  # color04 = "555555";
+  # color05 = "666666";
+  # color06 = "777777";
+  # color09 = "888888";
+  # color10 = "999999";
+  # color11 = "aaaaaa";
+  # color12 = "bbbbbb";
+  # color13 = "cccccc";
+  # color14 = "dddddd";
+
+  # color07 = "eeeeee";
+  # color15 = "ffffff";
+
+  # solarized dark
+  color00 = "002b36";
+  color08 = "073642";
+  color01 = "dc322f";
+  color02 = "859900";
+  color03 = "b58900";
+  color04 = "268bd2";
+  color05 = "d33682";
+  color06 = "2aa198";
+  color09 = "cb4b16";
+  color10 = "586e75";
+  color11 = "657b83";
+  color12 = "839496";
+  color13 = "6c71c4";
+  color14 = "93a1a1";
+  color07 = "eee8d5";
+  color15 = "fdf6e3";
 }
